4115501329b0664f7bc2318fc427b24ae21756c3
Paul Hankin <paul.hankin@pobox.com>
2022-12-19 12:32:40 +0100

    bad day 19

--- day19.go
+++ day19.go
@@ -91,79 +91,6 @@ func parse19(r io.Reader) ([]d19, error) {
 	return rr, s.Err()
 }
 
-type key19 struct {
-	robots        [4]int
-	ores          [4]int
-	timeRemaining int
-}
-
-func genKeys(bp *d19, r int, oldk, k key19, res *[]key19) {
-	if r == -1 {
-		return
-	}
-
-	canBuild := true
-	for i := 0; i < 4; i++ {
-		if oldk.ores[i] < bp.robot[r][i] {
-			canBuild = false
-		}
-	}
-	if !(canBuild && (r > 1 || k.robots[r] == 0)) {
-		genKeys(bp, r-1, oldk, k, res)
-	}
-
-	for k.robots[r] < 10 {
-		k.robots[r]++
-		for i := 0; i < 4; i++ {
-			k.ores[i] -= bp.robot[r][i]
-			oldk.ores[i] -= bp.robot[r][i]
-			if oldk.ores[i] < 0 {
-				return
-			}
-		}
-		*res = append(*res, k)
-		genKeys(bp, r-1, oldk, k, res)
-	}
-}
-
-func geodes(bp *d19, key key19, cache map[key19]int) int {
-	if i, ok := cache[key]; ok {
-		return i
-	}
-	nk := key
-	for i := 0; i < 4; i++ {
-		nk.ores[i] += nk.robots[i]
-	}
-	nk.timeRemaining--
-	if nk.timeRemaining == 0 {
-		cache[key] = nk.ores[geode]
-		return cache[key]
-	}
-	// fmt.Println(key)
-	best := 0
-	var keys []key19
-	canBuildAll := true
-	for r := 0; r < 4; r++ {
-		for i := 0; i < 4; i++ {
-			if key.ores[i] < bp.robot[r][i] {
-				canBuildAll = false
-			}
-		}
-	}
-	if !canBuildAll {
-		keys = append(keys, nk)
-	}
-	if nk.timeRemaining > 1 {
-		// don't build robots on the last turn
-		genKeys(bp, 3, key, nk, &keys)
-	}
-	for _, k := range keys {
-		best = max(best, geodes(bp, k, cache))
-	}
-	cache[key] = best
-	return best
-}
-
 func day19(fn string) (any, any, error) {
 	f, err := os.Open(fn)
 	if err != nil {
@@ -177,14 +104,6 @@ func day19(fn string) (any, any, error) {
 	part1 := 0
 	for _, blueprint := range dd {
 		fmt.Println(blueprint)
-		cache := map[key19]int{}
-		start := key19{
-			robots:        [4]int{1, 0, 0, 0},
-			timeRemaining: 24,
-		}
-		i := geodes(&blueprint, start, cache)
-		fmt.Println(blueprint.n, i)
-		part1 += i * blueprint.n
 	}
 	return part1, 0, nil
 }
